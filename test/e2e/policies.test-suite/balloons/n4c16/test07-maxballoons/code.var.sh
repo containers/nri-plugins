b89a23eab27d63adfc3c72c83d37814e65b904f0
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2023-01-17 12:00:27 +0200

    e2e: Add various balloons tests
    Add various balloons e2e tests that pass the test.
    Remaining tests to pass will be fixed in subsequent commits.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

---
+++ test/e2e/policies.test-suite/balloons/n4c16/test07-maxballoons/code.var.sh
@@ -0,0 +1,75 @@
+cleanup() {
+    vm-command "kubectl delete pods --all --now"
+    return 0
+}
+
+cleanup
+
+terminate nri-resmgr
+nri_resmgr_cfg=${TEST_DIR}/balloons-maxballoons.cfg launch nri-resmgr
+
+# pod0: allocate 1500/2000 mCPUs of the singleton balloon
+CPUREQ="1500m" CPULIM="1500m"
+POD_ANNOTATION="balloon.balloons.cri-resource-manager.intel.com: singleton" CONTCOUNT=1 create balloons-busybox
+report allowed
+verify 'len(cpus["pod0c0"]) == 2'
+
+# pod1: allocate the rest 500/2000 mCPUs of the singleton balloon
+CPUREQ="500m" CPULIM="500m"
+POD_ANNOTATION="balloon.balloons.cri-resource-manager.intel.com: singleton" CONTCOUNT=1 create balloons-busybox
+report allowed
+verify 'cpus["pod0c0"] == cpus["pod1c0"]'
+
+# pod2: try to fit in the already full singleton balloon
+CPUREQ="100m" CPULIM="100m"
+( POD_ANNOTATION="balloon.balloons.cri-resource-manager.intel.com: singleton" CONTCOUNT=1 wait_t=5s create balloons-busybox ) && {
+    error "creating pod2 succeeded but was expected to fail with balloon allocation error"
+}
+echo "pod2 creation failed with an error as expected"
+vm-command "kubectl describe pod pod2"
+if ! grep -q 'no suitable balloon instance available' <<< "$COMMAND_OUTPUT"; then
+    error "could not find 'no suitable balloon instance available' in pod2 description"
+fi
+vm-command "kubectl delete pod pod2 --now"
+
+# pod2: create dynamically the first dynamictwo balloon
+CPUREQ="800m" CPULIM="800m"
+POD_ANNOTATION="balloon.balloons.cri-resource-manager.intel.com: dynamictwo" CONTCOUNT=1 create balloons-busybox
+report allowed
+verify 'len(cpus["pod2c0"]) == 1'
+
+# pod3: create dynamically the second dynamictwo balloon
+CPUREQ="600m" CPULIM="600m"
+POD_ANNOTATION="balloon.balloons.cri-resource-manager.intel.com: dynamictwo" CONTCOUNT=1 create balloons-busybox
+report allowed
+verify 'disjoint_sets(cpus["pod2c0"], cpus["pod3c0"])'
+
+# pod4: prefering new balloon fails, but this fits in the second dynamictwo balloon
+CPUREQ="300m" CPULIM="300m"
+POD_ANNOTATION="balloon.balloons.cri-resource-manager.intel.com: dynamictwo" CONTCOUNT=1 create balloons-busybox
+report allowed
+verify 'cpus["pod4c0"] == cpus["pod3c0"]'
+
+# pod5: prefering new balloon fails, and fitting to existing dynamictwo balloons fails
+CPUREQ="300m" CPULIM="300m"
+( POD_ANNOTATION="balloon.balloons.cri-resource-manager.intel.com: dynamictwo" CONTCOUNT=1 wait_t=5s create balloons-busybox ) && {
+    error "creating pod6 succeeded but was expected to fail with balloon allocation error"
+}
+vm-command "kubectl describe pod pod5"
+if ! grep -q 'no suitable balloon instance available' <<< "$COMMAND_OUTPUT"; then
+    error "could not find 'no suitable balloon instance available' in pod6 description"
+fi
+vm-command "kubectl delete pod pod5 --now"
+
+cleanup
+
+# Try starting nri-resmgr with a configuration where MinBalloons and
+# MaxBalloons of the same balloon type contradict.
+terminate nri-resmgr
+( nri_resmgr_cfg=${TEST_DIR}/balloons-maxballoons-impossible.cfg launch nri-resmgr ) && {
+    error "starting nri-resmgr succeeded, but was expected to fail due to impossible static balloons"
+}
+echo "starting nri-resmgr with impossible static balloons configuration failed as expected"
+
+terminate nri-resmgr
+launch nri-resmgr
