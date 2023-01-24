59cfcedab6f870cd26606d1acdef01bfffcebce2
Muyassarov, Feruzjon <feruzjon.muyassarov@intel.com>
2023-01-24 19:36:57 +0200

    agent: implement node resource topology interface.
    Fill in the missing details for node resource topology
    CR management.
    
    Signed-off-by: Muyassarov, Feruzjon <feruzjon.muyassarov@intel.com>

--- pkg/agent/node-resource-topology.go
+++ pkg/agent/node-resource-topology.go
@@ -17,7 +17,14 @@ limitations under the License.
 package agent
 
 import (
+	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	policyapi "github.com/intel/nri-resmgr/pkg/policy"
+	nrtapi "github.com/k8stopologyawareschedwg/noderesourcetopology-api/pkg/apis/topology/v1alpha1"
 )
 
 // UpdateNrtCR updates the node's node resource topology CR using the given data.
@@ -29,6 +36,98 @@ func (a *agent) UpdateNrtCR(policy string, zones []*policyapi.TopologyZone) erro
 		return nil
 	}
 
-	a.Info("*** should export policy %s node resource topology CR for node %s...",
-		policy, nodeName)
+	cli := a.nrtCli.NodeResourceTopologies()
+	ctx := context.Background()
+	cr, err := cli.Get(ctx, nodeName, metav1.GetOptions{})
+	if err != nil {
+		cr = nil
+		if !errors.IsNotFound(err) {
+			a.Warn("failed to look up current node resource topology CR: %v", err)
+		}
+	}
+
+	// delete existing CR if we got no data from policy
+	// XXX TODO Deletion should be handled differently:
+	//   1. add expiration timestamp to nrtapi.NodeResourceTopology
+	//   2. GC CRs that are past their expiration time (for instance by NFD)
+	//   3. make sure we refresh our CR (either here or preferably/easier
+	//      by triggering in resmgr an updateTopologyZones() during longer
+	//      periods of inactivity)
+	if len(zones) == 0 {
+		if cr != nil {
+			err := cli.Delete(ctx, nodeName, metav1.DeleteOptions{})
+			if err != nil && !errors.IsNotFound(err) {
+				return fmt.Errorf("failed to delete node resource topology CR: %w", err)
+			}
+		}
+		return nil
+	}
+
+	// otherwise update CR if one exists
+	if cr != nil {
+		cr.TopologyPolicies = []string{
+			policy,
+		}
+		cr.Zones = zonesToNrt(zones)
+
+		_, err = cli.Update(ctx, cr, metav1.UpdateOptions{})
+		if err != nil {
+			return fmt.Errorf("failed to update node resource topology CR: %w", err)
+		}
+
+		return nil
+	}
+
+	// or create a new one
+	cr = &nrtapi.NodeResourceTopology{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: nodeName,
+		},
+		TopologyPolicies: []string{
+			policy,
+		},
+		Zones: zonesToNrt(zones),
+	}
+
+	_, err = cli.Create(ctx, cr, metav1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to create node resource topology CR: %w", err)
+	}
+
+	return nil
+}
+
+func zonesToNrt(in []*policyapi.TopologyZone) nrtapi.ZoneList {
+	out := nrtapi.ZoneList{}
+	for _, i := range in {
+		resources := nrtapi.ResourceInfoList{}
+		for _, r := range i.Resources {
+			resources = append(resources, nrtapi.ResourceInfo{
+				Name:        r.Name,
+				Capacity:    r.Capacity,
+				Allocatable: r.Allocatable,
+				Available:   r.Available,
+			})
+		}
+		out = append(out, nrtapi.Zone{
+			Name:       i.Name,
+			Type:       i.Type,
+			Parent:     i.Parent,
+			Resources:  resources,
+			Attributes: attributesToNrt(i.Attributes),
+		})
+	}
+	return out
+}
+
+func attributesToNrt(in []*policyapi.ZoneAttribute) nrtapi.AttributeList {
+	var out nrtapi.AttributeList
+	for _, i := range in {
+		out = append(out, nrtapi.AttributeInfo{
+			Name:  i.Name,
+			Value: i.Value,
+		})
+	}
+
+	return out
 }
