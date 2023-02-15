02d8e52dcee2995f8662c1c3312be3b2f56b2c18
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2023-02-15 14:17:55 +0200

    e2e: Add balloons ConfigMap test
    This is the test06-update-configmap test from cri-resource-manager
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

---
+++ test/e2e/policies.test-suite/balloons/n4c16/test06-update-configmap/code.var.sh
@@ -0,0 +1,79 @@
+# This test verifies that configuration updates via nri-resmgr-agent
+# are handled properly in the balloons policy.
+
+terminate nri-resmgr
+launch nri-resmgr
+
+testns=e2e-balloons-test06
+
+cleanup() {
+    vm-command "kubectl delete pods --all --now; \
+        kubectl delete pods -n $testns --all --now; \
+        kubectl delete pods -n btype1ns0 --all --now; \
+        kubectl delete namespace $testns || :; \
+        kubectl delete namespace btype1ns0 || :"
+    vm-port-forward-disable
+    terminate nri-resmgr
+}
+
+apply-configmap() {
+    vm-put-file $(instantiate balloons-configmap.yaml) balloons-configmap.yaml
+    vm-command "cat balloons-configmap.yaml"
+    vm-command "kubectl apply -f balloons-configmap.yaml"
+}
+
+cleanup
+nri_resmgr_extra_args="-metrics-interval 1s" nri_resmgr_config=fallback launch nri-resmgr
+
+vm-command "kubectl create namespace $testns"
+vm-command "kubectl create namespace btype1ns0"
+
+AVAILABLE_CPU="cpuset:0,4-15" BTYPE2_NAMESPACE0='"*"' BTYPE1_MAXCPUS='0' apply-configmap
+sleep 3
+vm-port-forward-enable
+
+# pod0 in btype0, annotation
+CPUREQ=1 MEMREQ="100M" CPULIM=1 MEMLIM="100M"
+POD_ANNOTATION="balloon.balloons.cri-resource-manager.intel.com: btype0" create balloons-busybox
+# pod1 in btype1, namespace
+CPUREQ=1 MEMREQ="100M" CPULIM=1 MEMLIM="100M"
+namespace="btype1ns0" create balloons-busybox
+# pod2 in btype2, wildcard namespace
+CPUREQ=1 MEMREQ="100M" CPULIM=1 MEMLIM="100M"
+namespace="e2e-balloons-test06" create balloons-busybox
+vm-command "curl -s $verify_metrics_url"
+verify-metrics-has-line 'btype0\[0\].*containers=".*pod0:pod0c0'
+verify-metrics-has-line 'btype1\[0\].*containers=".*pod1:pod1c0'
+verify-metrics-has-line 'btype2\[0\].*containers=".*pod2:pod2c0'
+
+# Remove first two balloon types, change btype2 to match all
+# namespaces.
+BTYPE0_SKIP=1 BTYPE1_SKIP=1 BTYPE2_NAMESPACE0='"*"' apply-configmap
+# Note:
+
+# pod0 was successfully assigned to and running in balloon of btype0.
+# Now btype0 was completely removed from the node.
+# Currently this behavior is undefined.
+# Possible behaviors: evict pod0, continue assign chain, refuse config...
+# For now, skip pod0c0 balloon validation:
+# verify-metrics-has-line '"btype2\[0\]".*pod0:pod0c0'
+verify-metrics-has-line '"btype2\[0\]".*pod1:pod1c0'
+verify-metrics-has-line '"btype2\[0\]".*pod2:pod2c0'
+
+# Bring back btype0 where pod0 belongs to by annotation.
+BTYPE1_SKIP=1 BTYPE2_NAMESPACE0='"*"' apply-configmap
+verify-metrics-has-line '"btype0\[0\]".*pod0:pod0c0'
+verify-metrics-has-line '"btype2\[0\]".*pod1:pod1c0'
+verify-metrics-has-line '"btype2\[0\]".*pod2:pod2c0'
+
+# Change only CPU classes, no reassigning.
+verify-metrics-has-line 'btype0\[0\].*pod0:pod0c0.*cpu_class="classA"'
+verify-metrics-has-line 'btype2\[0\].*pod1:pod1c0.*cpu_class="classC"'
+verify-metrics-has-line 'btype2\[0\].*pod2:pod2c0.*cpu_class="classC"'
+BTYPE0_CPUCLASS="classC" BTYPE1_SKIP=1 BTYPE2_CPUCLASS="classB" BTYPE2_NAMESPACE0='"*"'  apply-configmap
+verify-metrics-has-line 'btype0\[0\].*pod0:pod0c0.*cpu_class="classC"'
+verify-metrics-has-line 'btype2\[0\].*pod1:pod1c0.*cpu_class="classB"'
+verify-metrics-has-line 'btype2\[0\].*pod2:pod2c0.*cpu_class="classB"'
+
+cleanup
+launch nri-resmgr
