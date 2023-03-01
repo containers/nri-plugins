43d24a7714dd076bb103b84ff6add627a5c919f8
Muyassarov, Feruzjon <feruzjon.muyassarov@intel.com>
2023-03-01 17:21:08 +0200

    e2e: fix incorrect configmap name in TA plugin tests
    The name of the configMap file is missing .in at the end. Otherwise,
    instantiate function fails to find the file in the tree. Also, extend
    the readiness probe checking test so that we cover the situation when
    syntactically incorrect configuration is fixed and nri-rm pod becomes
    ready again.
    
    Signed-off-by: Muyassarov, Feruzjon <feruzjon.muyassarov@intel.com>

--- test/e2e/policies.test-suite/topology-aware/n4c16/test12-health-checking/code.var.sh
+++ test/e2e/policies.test-suite/topology-aware/n4c16/test12-health-checking/code.var.sh
@@ -1,17 +1,31 @@
 # Test that
 # Syntatically incorrect configuration file is resulting in the nri-resmgr
 # pod readiness probe failure.
+dummyData="foo: bar"
 vm-put-file $(instantiate nri-resmgr-configmap.yaml) nri-resmgr-configmap.yaml
 
 terminate nri-resmgr
-launch nri-resmgr
+nri_resmgr_config=fallback launch nri-resmgr
 vm-command "kubectl apply -f nri-resmgr-configmap.yaml"
 
-# Give enough time for the Kubernetes to consider the container unhealthy
+# Give enough time for the Kubernetes to consider the container not ready.
 sleep 10
 
 # Check if nri-resmgr readiness probe failed as expected
 if ! vm-command "kubectl get event -n kube-system --field-selector involvedObject.name=$POD | grep -q 'Readiness probe failed'" 2>&1; then
-    error "Expected liveness probe to fail, but got it succeeded"
+    error "Expected readiness probe to fail, but got it succeeded"
 fi
-echo "nri-resmgr health-check failed as expected"
+echo "nri-resmgr readiness probe failed as expected"
+
+# Fix the incorrect data in the configuration and check if nri-rm becomes ready.
+dummyData=""
+vm-put-file $(instantiate nri-resmgr-configmap.yaml) nri-resmgr-configmap.yaml
+vm-command "kubectl apply -f nri-resmgr-configmap.yaml"
+
+sleep 10
+
+if ! vm-command "kubectl get event -n kube-system --field-selector involvedObject.name=$POD | grep -q 'Readiness probe failed'" 2>&1; then
+    echo "nri-resmgr readiness probe succeeded as expected"
+else
+    error "Expected readiness probe to succeed, but it failed"
+fi
