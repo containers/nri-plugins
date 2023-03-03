b218024cab38465e1ee96d746ce7eaaff6af8fed
Muyassarov, Feruzjon <feruzjon.muyassarov@intel.com>
2023-03-03 04:40:33 +0200

    e2e: fetch the nri-rm pod readiness from status
    Currently, as a source of truth for considering container as running
    or not, we are grepping through kubectl describe... result for a
    particular string, i.e. Readiness probe failed. But that becomes
    easily problematic when container is actually running but the kubectl
    described result still has the old data about failed readiness. In other
    words, the readiness probe failure message doesn't get removed from
    kubectl describe instantly when container switches from not ready to
    ready. As such, instead use kubectl wait... with it's timeout flag
    via vm-wait-pod-regexp function.
    
    Signed-off-by: Muyassarov, Feruzjon <feruzjon.muyassarov@intel.com>

--- test/e2e/policies.test-suite/balloons/n4c16/test10-health-checking/code.var.sh
+++ test/e2e/policies.test-suite/balloons/n4c16/test10-health-checking/code.var.sh
@@ -6,24 +6,21 @@ vm-put-file $(instantiate nri-resmgr-configmap.yaml) nri-resmgr-configmap.yaml
 
 terminate nri-resmgr
 nri_resmgr_config=fallback launch nri-resmgr
+
+# Check that nri-resmgr readiness probe fails as expected
 vm-command "kubectl apply -f nri-resmgr-configmap.yaml"
-
-# Give enough time for the Kubernetes to consider the container not ready.
-sleep 10
-
-# Check if nri-resmgr readiness probe failed as expected
-if ! vm-command "kubectl get event -n kube-system --field-selector involvedObject.name=$POD | grep -q 'Readiness probe failed'" 2>&1; then
-    error "Expected readiness probe to fail, but got it succeeded"
+namespace=kube-system wait=Ready=false wait_t=60 vm-wait-pod-regexp nri-resmgr-
+if [ $ret -ne 0 ]; then
+    error "Expected readiness probe to fail, but got it succeeded..."
 fi
-echo "nri-resmgr readiness probe failed as expected"
+echo "nri-resmgr readiness probe failed as expected..."
 
-# Fix the incorrect data in the configuration and check if nri-rm becomes ready.
+# Fix the incorrect data in the configuration and check if nri-rm becomes ready
 dummyData=""
 vm-put-file $(instantiate nri-resmgr-configmap.yaml) nri-resmgr-configmap.yaml
-
-sleep 5
-
-if vm-command "kubectl get event -n kube-system --field-selector involvedObject.name=$POD | grep -q 'Readiness probe failed'" 2>&1; then
-    error "Expected readiness probe to succeed, but it failed"
+vm-command "kubectl apply -f nri-resmgr-configmap.yaml"
+namespace=kube-system wait=Ready=true wait_t=60 vm-wait-pod-regexp nri-resmgr-
+if [ $ret -ne 0 ]; then
+    error "Expected readiness probe to succeed, but it failed..."
 fi
-echo "nri-resmgr readiness probe succeeded as expected"
+echo "nri-resmgr readiness probe succeeded as expected..."
