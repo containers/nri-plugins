jukkar/nri-resmgr

docs: Generate documentation
Add documentation files and makefile rules to generate html
documentation for the project.

This is taken from cri-resource-manager and HEAD was at
commit 5db1e8a5fc0ec15780f5ffa6fbb2a9d826cb221b

Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

Makefile
@@ -16,6 +16,9 @@ SHELL := /bin/bash
 # Kubernetes version we pull in as modules and our external API versions.
 KUBERNETES_VERSION := $(shell grep 'k8s.io/kubernetes ' go.mod | sed 's/^.* //')
 
+PREFIX     ?= /usr
+DOCDIR     ?= $(PREFIX)/share/doc/nri-resmgr
+
 # Directories (in cmd) with go code we'll want to create Docker images from.
 IMAGE_DIRS  = $(shell find cmd -name Dockerfile | sed 's:cmd/::g;s:/.*::g' | uniq)
 IMAGE_VERSION  := $(shell git describe --dirty 2> /dev/null || echo unknown)
@@ -83,6 +86,17 @@ LDFLAGS    = \
     -ldflags "$(STATIC_LDFLAGS) -X=github.com/intel/nri-resmgr/pkg/version.Version=$(BUILD_VERSION) \
              -X=github.com/intel/nri-resmgr/pkg/version.Build=$(BUILD_BUILDID) \
              -B 0x$(RANDOM_ID)"
+
+# Documentation-related variables
+SPHINXOPTS    ?= -W
+SPHINXBUILD   = sphinx-build
+SITE_BUILDDIR ?= build/docs
+
+# Docker base command for working with html documentation.
+DOCKER_SITE_BUILDER_IMAGE := nri-resmgr-site-builder
+DOCKER_SITE_CMD := $(DOCKER) run --rm -v "`pwd`:/docs" --user=`id -u`:`id -g` \
+	-p 8081:8081 \
+	-e SITE_BUILDDIR=$(SITE_BUILDDIR) -e SPHINXOPTS=$(SPHINXOPTS)
 
 #
 # top-level targets
@@ -273,3 +287,53 @@ pkg/sysfs/sst_types%.go: pkg/sysfs/_sst_types%.go pkg/sysfs/gen_sst_types.sh
 	$(Q)cd $(@D) && \
 	    KERNEL_SRC_DIR=$(KERNEL_SRC_DIR) $(GO_GEN)
 
+install-minimal-docs:
+	$(Q)echo "Installing minimal documentation to $(DOCDIR)..."; \
+	$(INSTALL) -d $(DESTDIR)$(DOCDIR) && \
+	for f in LICENSE docs/security.md; do \
+	    echo "  $$f in $(DESTDIR)$(DOCDIR)..."; \
+	    df=$${f##*/}; \
+	    $(INSTALL) -m 0644 -T $$f $(DESTDIR)$(DOCDIR)/$${df}; \
+	done
+
+#
+# Rules for documentation
+#
+
+vhtml: _work/venv/.stamp
+	. _work/venv/bin/activate && \
+		make -C docs html && \
+		cp -r docs/_build .
+
+html: clean-html
+	$(Q)BUILD_VERSION=$(BUILD_VERSION) \
+		$(SPHINXBUILD) -c docs docs "$(SITE_BUILDDIR)" $(SPHINXOPTS)
+	cp docs/index.html "$(SITE_BUILDDIR)"
+	for d in $$(find docs -name figures -type d); do \
+	    mkdir -p $(SITE_BUILDDIR)/$$d && cp $$d/* $(SITE_BUILDDIR)/$$d; \
+	done
+
+serve-html: html
+	$(Q)cd $(SITE_BUILDDIR) && python3 -m http.server 8081
+
+clean-html:
+	rm -rf $(SITE_BUILDDIR)
+
+site-build: .$(DOCKER_SITE_BUILDER_IMAGE).image.stamp
+	$(Q)$(DOCKER_SITE_CMD) $(DOCKER_SITE_BUILDER_IMAGE) make html
+
+site-serve: .$(DOCKER_SITE_BUILDER_IMAGE).image.stamp
+	$(Q)$(DOCKER_SITE_CMD) -it $(DOCKER_SITE_BUILDER_IMAGE) make serve-html
+
+.$(DOCKER_SITE_BUILDER_IMAGE).image.stamp: docs/Dockerfile docs/requirements.txt
+	docker build -t $(DOCKER_SITE_BUILDER_IMAGE) docs
+	touch $@
+
+# Set up a Python3 environment with the necessary tools for document creation.
+_work/venv/.stamp: docs/requirements.txt
+	rm -rf ${@D}
+	python3 -m venv ${@D}
+	. ${@D}/bin/activate && pip install -r $<
+	touch $@
+
+docs: site-build

docs/conf.py
@@ -1,0 +1,283 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+from docutils import nodes
+from os.path import isdir, isfile, join, basename, dirname
+from os import makedirs, getenv
+from shutil import copyfile
+from subprocess import run, STDOUT
+
+# -- Project information -----------------------------------------------------
+
+project = 'CRI Resource Manager'
+copyright = '2020, various'
+author = 'various'
+
+master_doc = 'docs/index'
+
+
+##############################################################################
+#
+# This section determines the behavior of links to local items in .md files.
+#
+#  if useGitHubURL == True:
+#
+#     links to local files and directories will be turned into github URLs
+#     using either the baseBranch defined here or using the commit SHA.
+#
+#  if useGitHubURL == False:
+#
+#     local files will be moved to the website directory structure when built
+#     local directories will still be links to github URLs
+#
+#  if built with GitHub workflows:
+#
+#     the GitHub URLs will use the commit SHA (GITHUB_SHA environment variable
+#     is defined by GitHub workflows) to link to the specific commit.
+#
+##############################################################################
+
+baseBranch = "master"
+useGitHubURL = True
+commitSHA = getenv('GITHUB_SHA')
+githubServerURL = getenv('GITHUB_SERVER_URL')
+githubRepository = getenv('GITHUB_REPOSITORY')
+if githubServerURL and githubRepository:
+    githubBaseURL = join(githubServerURL, githubRepository)
+else:
+    githubBaseURL = "https://github.com/intel/cri-resource-manager/"
+
+githubFileURL = join(githubBaseURL, "blob/")
+githubDirURL = join(githubBaseURL, "tree/")
+if commitSHA:
+    githubFileURL = join(githubFileURL, commitSHA)
+    githubDirURL = join(githubDirURL, commitSHA)
+else:
+    githubFileURL = join(githubFileURL, baseBranch)
+    githubDirURL = join(githubDirURL, baseBranch)
+
+# Version displayed in the upper left corner of the site
+ref = getenv('GITHUB_REF', default="")
+if ref == "refs/heads/master":
+    version = "devel"
+elif ref.startswith("refs/heads/release-"):
+    # For release branches just show the latest tag name
+    buildVersion = getenv("BUILD_VERSION", default="unknown")
+    version = buildVersion.split('-')[0]
+elif ref.startswith("refs/tags/"):
+    version = ref[len("refs/tags/"):]
+else:
+    version = getenv("BUILD_VERSION", default="unknown")
+
+release = getenv("BUILD_VERSION", default="unknown")
+
+# Versions to show in the version menu
+if getenv('VERSIONS_MENU'):
+    html_context = {
+        'versions_menu': True,
+        'versions_menu_this_version': getenv('VERSIONS_MENU_THIS_VERSION', version)}
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['myst_parser', 'sphinx_markdown_tables']
+myst_enable_extensions = ['substitution']
+source_suffix = {'.rst': 'restructuredtext','.md': 'markdown'}
+
+# Substitution variables
+def module_version(module, version):
+    version=version.split('-', 1)[0]
+    if module == 'github.com/intel/goresctrl':
+        version = '.'.join(version.split('.')[0:2]) + '.0'
+    return version
+
+def gomod_versions(modules):
+    versions = {}
+    gocmd = run(['go', 'list', '-m', '-f', '{{.GoVersion}}'],
+                check=True, capture_output=True, universal_newlines=True)
+    versions['golang'] = gocmd.stdout.strip()
+    for m in modules:
+        gocmd = run(['go', 'list', '-m', '-f', '{{.Version}}', '%s' % m],
+                    check=True, capture_output=True, universal_newlines=True)
+        versions[m] = module_version(m, gocmd.stdout.strip())
+    return versions
+
+mod_versions = gomod_versions(['github.com/intel/goresctrl'])
+myst_substitutions = {
+    'golang_version': mod_versions['golang'],
+    'goresctrl_version': mod_versions['github.com/intel/goresctrl']
+}
+myst_heading_anchors = 3
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', '.github', '_work', 'generate', 'README.md', 'docs/releases']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    'display_version': True,
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+#html_static_path = ['_static']
+
+def setup(app):
+    app.connect('doctree-resolved',fixLocalMDAnchors)
+    app.connect('missing-reference',fixRSTLinkInMD)
+
+###############################################################################
+#
+#  This section defines callbacks that make markdown specific tweaks to
+#  either:
+#
+#  1. Fix something that recommonmark does wrong.
+#  2. Provide support for .md files that are written as READMEs in a GitHub
+#     repo.
+#
+#  Only use these changes if using the extension ``recommonmark``.
+#
+###############################################################################
+
+def isHTTPLink(uri):
+    return uri.startswith('http://') or uri.startswith('https://')
+
+def isMDFileLink(uri):
+    return uri.endswith('.md') or '.md#' in uri
+
+def isRSTFileLink(uri):
+    return uri.endswith('.rst')
+
+# Callback registerd with 'missing-reference'.
+def fixRSTLinkInMD(app, env, node, contnode):
+    refTarget = node.get('reftarget')
+
+    if isHTTPLink(refTarget):
+        return
+
+    if isRSTFileLink(refTarget) and not isHTTPLink(refTarget):
+    # This occurs when a .rst file is referenced from a .md file
+    # Currently unable to check if file exists as no file
+    # context is provided and links are relative.
+    #
+    # Example: [Application examples](examples/readme.rst)
+    #
+        contnode['refuri'] = contnode['refuri'].replace('.rst','.html')
+        contnode['internal'] = "True"
+        return contnode
+    elif refTarget.startswith("/"):
+    # This occurs when a file is referenced for download from an .md file.
+    # Construct a list of them and short-circuit the warning. The files
+    # are moved later (need file location context). To avoid warnings,
+    # write .md files, make the links absolute. This only marks them fixed
+    # if it can verify that they exist.
+    #
+    # Example: [Makefile](/Makefile)
+    #
+        filePath = refTarget.lstrip("/")
+        if isfile(filePath) or isdir(filePath):
+            return contnode
+
+
+def normalizePath(docPath,uriPath):
+    if uriPath == "":
+        return uriPath
+    if "#" in uriPath:
+    # Strip out anchors
+        uriPath = uriPath.split("#")[0]
+    if uriPath.startswith("/"):
+    # It's an absolute path
+        return uriPath.lstrip("/") #path to file from project directory
+    else:
+    # It's a relative path
+        docDir = dirname(docPath)
+        return join(docDir,uriPath) #path to file from referencing file
+
+
+# Callback registerd with 'doctree-resolved'.
+def fixLocalMDAnchors(app, doctree, docname):
+    for node in doctree.traverse(nodes.reference):
+        uri = node.get('refuri')
+
+        if isHTTPLink(uri):
+            continue
+
+        filePath = normalizePath(docname,uri)
+
+        if isfile(filePath):
+        # Only do this if the file exists.
+        #
+        # TODO: Pop a warning if the file doesn't exist.
+        #
+            if isMDFileLink(uri) and not isHTTPLink(uri):
+            # Make sure .md file links that weren't caught are converted.
+            # These occur when creating an explicit link to an .md file
+            # from an .rst file. By default these are not validated by Sphinx
+            # or recommonmark. Only toctree references are validated. recommonmark
+            # also fails to convert links to local Markdown files that include
+            # anchors. This fixes that as well.
+            #
+            # Only include this code if .md files are being converted to html
+            #
+            # Example: `Google Cloud Engine <gce.md>`__
+            #          [configuration options](autotest.md#configuration-options)
+            #
+                node['refuri'] = node['refuri'].replace('.md','.html')
+            else:
+            # Handle the case where markdown is referencing local files in the repo
+            #
+            # Example: [Makefile](/Makefile)
+            #
+                if useGitHubURL:
+                # Replace references to local files with links to the GitHub repo
+                #
+                    newURI = join(githubFileURL, filePath)
+                    print("new url: ", newURI)
+                    node['refuri']=newURI
+                else:
+                # If there are links to local files other than .md (.rst files are caught
+                # when warnings are fired), move the files into the Sphinx project, so
+                # they can be accessed.
+                    newFileDir = join(app.outdir,dirname(filePath)) # where to move the file in Sphinx output.
+                    newFilePath = join(app.outdir,filePath)
+                    newURI = uri # if the path is relative no need to change it.
+                    if uri.startswith("/"):
+                    # It's an absolute path. Need to make it relative.
+                        uri = uri.lstrip("/")
+                        docDirDepth = len(docname.split("/")) - 1
+                        newURI = "../"*docDirDepth + uri
+                    if not isdir(newFileDir):
+                        makedirs(newFileDir)
+                    copyfile(filePath,newFilePath)
+                    node['refuri'] = newURI
+        elif "#" not in uri: # ignore anchors
+        # turn links to directories into links to the repo
+            if isdir(filePath):
+                newURI = join(githubDirURL, filePath)
+                node['refuri']=newURI

containers/nri-plugins

Makefile
@@ -91,6 +91,17 @@ LDFLAGS    = \
              -X=github.com/containers/nri-plugins/pkg/version.Build=$(BUILD_BUILDID) \
              -B 0x$(RANDOM_ID)"
 
+# Documentation-related variables
+SPHINXOPTS    ?= -W
+SPHINXBUILD   = sphinx-build
+SITE_BUILDDIR ?= build/docs
+
+# Docker base command for working with html documentation.
+DOCKER_SITE_BUILDER_IMAGE := nri-plugins-site-builder
+DOCKER_SITE_CMD := $(DOCKER) run --rm -v "`pwd`:/docs" --user=`id -u`:`id -g` \
+	-p 8081:8081 \
+	-e SITE_BUILDDIR=$(SITE_BUILDDIR) -e SPHINXOPTS=$(SPHINXOPTS)
+
 #
 # top-level targets
 #
@@ -329,3 +340,33 @@ report-licenses:
 	        --ignore github.com/containers/nri-plugins \
 	        > $(LICENSE_PATH)/licenses.csv && \
 	echo See $(LICENSE_PATH)/licenses.csv for license information
+
+#
+# Rules for documentation
+#
+
+html: clean-html
+	$(Q)BUILD_VERSION=$(BUILD_VERSION) \
+		$(SPHINXBUILD) -c docs docs "$(SITE_BUILDDIR)" $(SPHINXOPTS)
+	cp docs/index.html "$(SITE_BUILDDIR)"
+	for d in $$(find docs -name figures -type d); do \
+	    mkdir -p $(SITE_BUILDDIR)/$$d && cp $$d/* $(SITE_BUILDDIR)/$$d; \
+	done
+
+serve-html: html
+	$(Q)cd $(SITE_BUILDDIR) && python3 -m http.server 8081
+
+clean-html:
+	rm -rf $(SITE_BUILDDIR)
+
+site-build: .$(DOCKER_SITE_BUILDER_IMAGE).image.stamp
+	$(Q)$(DOCKER_SITE_CMD) $(DOCKER_SITE_BUILDER_IMAGE) make html
+
+site-serve: .$(DOCKER_SITE_BUILDER_IMAGE).image.stamp
+	$(Q)$(DOCKER_SITE_CMD) -it $(DOCKER_SITE_BUILDER_IMAGE) make serve-html
+
+.$(DOCKER_SITE_BUILDER_IMAGE).image.stamp: docs/Dockerfile docs/requirements.txt
+	docker build -t $(DOCKER_SITE_BUILDER_IMAGE) docs
+	touch $@
+
+docs: site-build

docs/conf.py
@@ -1,0 +1,283 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+from docutils import nodes
+from os.path import isdir, isfile, join, basename, dirname
+from os import makedirs, getenv
+from shutil import copyfile
+from subprocess import run, STDOUT
+
+# -- Project information -----------------------------------------------------
+
+project = 'NRI Plugin Collection'
+copyright = '2023, various'
+author = 'various'
+
+master_doc = 'docs/index'
+
+
+##############################################################################
+#
+# This section determines the behavior of links to local items in .md files.
+#
+#  if useGitHubURL == True:
+#
+#     links to local files and directories will be turned into github URLs
+#     using either the baseBranch defined here or using the commit SHA.
+#
+#  if useGitHubURL == False:
+#
+#     local files will be moved to the website directory structure when built
+#     local directories will still be links to github URLs
+#
+#  if built with GitHub workflows:
+#
+#     the GitHub URLs will use the commit SHA (GITHUB_SHA environment variable
+#     is defined by GitHub workflows) to link to the specific commit.
+#
+##############################################################################
+
+baseBranch = "main"
+useGitHubURL = True
+commitSHA = getenv('GITHUB_SHA')
+githubServerURL = getenv('GITHUB_SERVER_URL')
+githubRepository = getenv('GITHUB_REPOSITORY')
+if githubServerURL and githubRepository:
+    githubBaseURL = join(githubServerURL, githubRepository)
+else:
+    githubBaseURL = "https://github.com/containers/nri-plugins/"
+
+githubFileURL = join(githubBaseURL, "blob/")
+githubDirURL = join(githubBaseURL, "tree/")
+if commitSHA:
+    githubFileURL = join(githubFileURL, commitSHA)
+    githubDirURL = join(githubDirURL, commitSHA)
+else:
+    githubFileURL = join(githubFileURL, baseBranch)
+    githubDirURL = join(githubDirURL, baseBranch)
+
+# Version displayed in the upper left corner of the site
+ref = getenv('GITHUB_REF', default="")
+if ref == "refs/heads/main":
+    version = "devel"
+elif ref.startswith("refs/heads/release-"):
+    # For release branches just show the latest tag name
+    buildVersion = getenv("BUILD_VERSION", default="unknown")
+    version = buildVersion.split('-')[0]
+elif ref.startswith("refs/tags/"):
+    version = ref[len("refs/tags/"):]
+else:
+    version = getenv("BUILD_VERSION", default="unknown")
+
+release = getenv("BUILD_VERSION", default="unknown")
+
+# Versions to show in the version menu
+if getenv('VERSIONS_MENU'):
+    html_context = {
+        'versions_menu': True,
+        'versions_menu_this_version': getenv('VERSIONS_MENU_THIS_VERSION', version)}
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['myst_parser', 'sphinx_markdown_tables']
+myst_enable_extensions = ['substitution']
+source_suffix = {'.rst': 'restructuredtext','.md': 'markdown'}
+
+# Substitution variables
+def module_version(module, version):
+    version=version.split('-', 1)[0]
+    if module == 'github.com/intel/goresctrl':
+        version = '.'.join(version.split('.')[0:2]) + '.0'
+    return version
+
+def gomod_versions(modules):
+    versions = {}
+    gocmd = run(['go', 'list', '-m', '-f', '{{.GoVersion}}'],
+                check=True, capture_output=True, universal_newlines=True)
+    versions['golang'] = gocmd.stdout.strip()
+    for m in modules:
+        gocmd = run(['go', 'list', '-m', '-f', '{{.Version}}', '%s' % m],
+                    check=True, capture_output=True, universal_newlines=True)
+        versions[m] = module_version(m, gocmd.stdout.strip())
+    return versions
+
+mod_versions = gomod_versions(['github.com/intel/goresctrl'])
+myst_substitutions = {
+    'golang_version': mod_versions['golang'],
+    'goresctrl_version': mod_versions['github.com/intel/goresctrl']
+}
+myst_heading_anchors = 3
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', '.github', '_work', 'generate', 'README.md', 'TODO.md', 'SECURITY.md', 'CODE-OF-CONDUCT.md', 'docs/releases', 'test/self-hosted-runner/README.md', 'test/e2e/README.md']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    'display_version': True,
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+#html_static_path = ['_static']
+
+def setup(app):
+    app.connect('doctree-resolved',fixLocalMDAnchors)
+    app.connect('missing-reference',fixRSTLinkInMD)
+
+###############################################################################
+#
+#  This section defines callbacks that make markdown specific tweaks to
+#  either:
+#
+#  1. Fix something that recommonmark does wrong.
+#  2. Provide support for .md files that are written as READMEs in a GitHub
+#     repo.
+#
+#  Only use these changes if using the extension ``recommonmark``.
+#
+###############################################################################
+
+def isHTTPLink(uri):
+    return uri.startswith('http://') or uri.startswith('https://')
+
+def isMDFileLink(uri):
+    return uri.endswith('.md') or '.md#' in uri
+
+def isRSTFileLink(uri):
+    return uri.endswith('.rst')
+
+# Callback registerd with 'missing-reference'.
+def fixRSTLinkInMD(app, env, node, contnode):
+    refTarget = node.get('reftarget')
+
+    if isHTTPLink(refTarget):
+        return
+
+    if isRSTFileLink(refTarget) and not isHTTPLink(refTarget):
+    # This occurs when a .rst file is referenced from a .md file
+    # Currently unable to check if file exists as no file
+    # context is provided and links are relative.
+    #
+    # Example: [Application examples](examples/readme.rst)
+    #
+        contnode['refuri'] = contnode['refuri'].replace('.rst','.html')
+        contnode['internal'] = "True"
+        return contnode
+    elif refTarget.startswith("/"):
+    # This occurs when a file is referenced for download from an .md file.
+    # Construct a list of them and short-circuit the warning. The files
+    # are moved later (need file location context). To avoid warnings,
+    # write .md files, make the links absolute. This only marks them fixed
+    # if it can verify that they exist.
+    #
+    # Example: [Makefile](/Makefile)
+    #
+        filePath = refTarget.lstrip("/")
+        if isfile(filePath) or isdir(filePath):
+            return contnode
+
+
+def normalizePath(docPath,uriPath):
+    if uriPath == "":
+        return uriPath
+    if "#" in uriPath:
+    # Strip out anchors
+        uriPath = uriPath.split("#")[0]
+    if uriPath.startswith("/"):
+    # It's an absolute path
+        return uriPath.lstrip("/") #path to file from project directory
+    else:
+    # It's a relative path
+        docDir = dirname(docPath)
+        return join(docDir,uriPath) #path to file from referencing file
+
+
+# Callback registerd with 'doctree-resolved'.
+def fixLocalMDAnchors(app, doctree, docname):
+    for node in doctree.traverse(nodes.reference):
+        uri = node.get('refuri')
+
+        if isHTTPLink(uri):
+            continue
+
+        filePath = normalizePath(docname,uri)
+
+        if isfile(filePath):
+        # Only do this if the file exists.
+        #
+        # TODO: Pop a warning if the file doesn't exist.
+        #
+            if isMDFileLink(uri) and not isHTTPLink(uri):
+            # Make sure .md file links that weren't caught are converted.
+            # These occur when creating an explicit link to an .md file
+            # from an .rst file. By default these are not validated by Sphinx
+            # or recommonmark. Only toctree references are validated. recommonmark
+            # also fails to convert links to local Markdown files that include
+            # anchors. This fixes that as well.
+            #
+            # Only include this code if .md files are being converted to html
+            #
+            # Example: `Google Cloud Engine <gce.md>`__
+            #          [configuration options](autotest.md#configuration-options)
+            #
+                node['refuri'] = node['refuri'].replace('.md','.html')
+            else:
+            # Handle the case where markdown is referencing local files in the repo
+            #
+            # Example: [Makefile](/Makefile)
+            #
+                if useGitHubURL:
+                # Replace references to local files with links to the GitHub repo
+                #
+                    newURI = join(githubFileURL, filePath)
+                    print("new url: ", newURI)
+                    node['refuri']=newURI
+                else:
+                # If there are links to local files other than .md (.rst files are caught
+                # when warnings are fired), move the files into the Sphinx project, so
+                # they can be accessed.
+                    newFileDir = join(app.outdir,dirname(filePath)) # where to move the file in Sphinx output.
+                    newFilePath = join(app.outdir,filePath)
+                    newURI = uri # if the path is relative no need to change it.
+                    if uri.startswith("/"):
+                    # It's an absolute path. Need to make it relative.
+                        uri = uri.lstrip("/")
+                        docDirDepth = len(docname.split("/")) - 1
+                        newURI = "../"*docDirDepth + uri
+                    if not isdir(newFileDir):
+                        makedirs(newFileDir)
+                    copyfile(filePath,newFilePath)
+                    node['refuri'] = newURI
+        elif "#" not in uri: # ignore anchors
+        # turn links to directories into links to the repo
+            if isdir(filePath):
+                newURI = join(githubDirURL, filePath)
+                node['refuri']=newURI