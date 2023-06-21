9862cc79fff45d86a3ea4e5b8bceaf8a4055970f
Markus Lehtonen <markus.lehtonen@intel.com>
2023-06-21 14:48:35 +0300

    docs: fix references to github repo
    This relies on the myst parser's (new in v0.19) feature of customizing
    external URL schemes with the "myst_url_schemes" configuration option.
    We specify new URL schemes "blob" and "tree" which are supposed to be
    used for references to blobs (files) and trees (directories) in the git
    repo.
    
    This also drops sphinx event hooks ("doctree-resolved" and
    "missing-reference"), simplifying the conf.py considerably. They were
    useless anyway as they were totally broken and didn't actually fix any
    references to repo files.
    
    The downside of this is that the affected references are not shown when
    browsing .md files directly from github (as github dosn't handle our
    custom 'tree:' and 'blob:' url schemes. However, it should be preferable
    to have the cross-references working in the official documentation.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@intel.com>

--- docs/conf.py
+++ docs/conf.py
@@ -32,16 +32,6 @@ master_doc = 'docs/index'
 #
 # This section determines the behavior of links to local items in .md files.
 #
-#  if useGitHubURL == True:
-#
-#     links to local files and directories will be turned into github URLs
-#     using either the baseBranch defined here or using the commit SHA.
-#
-#  if useGitHubURL == False:
-#
-#     local files will be moved to the website directory structure when built
-#     local directories will still be links to github URLs
-#
 #  if built with GitHub workflows:
 #
 #     the GitHub URLs will use the commit SHA (GITHUB_SHA environment variable
@@ -50,7 +40,6 @@ master_doc = 'docs/index'
 ##############################################################################
 
 baseBranch = "main"
-useGitHubURL = True
 commitSHA = getenv('GITHUB_SHA')
 githubServerURL = getenv('GITHUB_SERVER_URL')
 githubRepository = getenv('GITHUB_REPOSITORY')
@@ -123,6 +112,15 @@ myst_substitutions = {
 }
 myst_heading_anchors = 3
 
+myst_url_schemes = {
+    "http": None,
+    "https": None,
+    "ftp": None,
+    "mailto": None,
+    "blob": githubFileURL + "{{path}}",
+    "tree": githubDirURL + "{{path}}",
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -141,142 +139,3 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     'display_version': True,
 }
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
-
-def setup(app):
-    app.connect('doctree-resolved',fixLocalMDAnchors)
-    app.connect('missing-reference',fixRSTLinkInMD)
-
-###############################################################################
-#
-#  This section defines callbacks that make markdown specific tweaks to
-#  either:
-#
-#  1. Fix something that recommonmark does wrong.
-#  2. Provide support for .md files that are written as READMEs in a GitHub
-#     repo.
-#
-#  Only use these changes if using the extension ``recommonmark``.
-#
-###############################################################################
-
-def isHTTPLink(uri):
-    return uri.startswith('http://') or uri.startswith('https://')
-
-def isMDFileLink(uri):
-    return uri.endswith('.md') or '.md#' in uri
-
-def isRSTFileLink(uri):
-    return uri.endswith('.rst')
-
-# Callback registerd with 'missing-reference'.
-def fixRSTLinkInMD(app, env, node, contnode):
-    refTarget = node.get('reftarget')
-
-    if isHTTPLink(refTarget):
-        return
-
-    if isRSTFileLink(refTarget) and not isHTTPLink(refTarget):
-    # This occurs when a .rst file is referenced from a .md file
-    # Currently unable to check if file exists as no file
-    # context is provided and links are relative.
-    #
-    # Example: [Application examples](examples/readme.rst)
-    #
-        contnode['refuri'] = contnode['refuri'].replace('.rst','.html')
-        contnode['internal'] = "True"
-        return contnode
-    elif refTarget.startswith("/"):
-    # This occurs when a file is referenced for download from an .md file.
-    # Construct a list of them and short-circuit the warning. The files
-    # are moved later (need file location context). To avoid warnings,
-    # write .md files, make the links absolute. This only marks them fixed
-    # if it can verify that they exist.
-    #
-    # Example: [Makefile](/Makefile)
-    #
-        filePath = refTarget.lstrip("/")
-        if isfile(filePath) or isdir(filePath):
-            return contnode
-
-
-def normalizePath(docPath,uriPath):
-    if uriPath == "":
-        return uriPath
-    if "#" in uriPath:
-    # Strip out anchors
-        uriPath = uriPath.split("#")[0]
-    if uriPath.startswith("/"):
-    # It's an absolute path
-        return uriPath.lstrip("/") #path to file from project directory
-    else:
-    # It's a relative path
-        docDir = dirname(docPath)
-        return join(docDir,uriPath) #path to file from referencing file
-
-
-# Callback registerd with 'doctree-resolved'.
-def fixLocalMDAnchors(app, doctree, docname):
-    for node in doctree.traverse(nodes.reference):
-        uri = node.get('refuri')
-
-        if uri == None or isHTTPLink(uri):
-            continue
-
-        filePath = normalizePath(docname,uri)
-
-        if isfile(filePath):
-        # Only do this if the file exists.
-        #
-        # TODO: Pop a warning if the file doesn't exist.
-        #
-            if isMDFileLink(uri) and not isHTTPLink(uri):
-            # Make sure .md file links that weren't caught are converted.
-            # These occur when creating an explicit link to an .md file
-            # from an .rst file. By default these are not validated by Sphinx
-            # or recommonmark. Only toctree references are validated. recommonmark
-            # also fails to convert links to local Markdown files that include
-            # anchors. This fixes that as well.
-            #
-            # Only include this code if .md files are being converted to html
-            #
-            # Example: `Google Cloud Engine <gce.md>`__
-            #          [configuration options](autotest.md#configuration-options)
-            #
-                node['refuri'] = node['refuri'].replace('.md','.html')
-            else:
-            # Handle the case where markdown is referencing local files in the repo
-            #
-            # Example: [Makefile](/Makefile)
-            #
-                if useGitHubURL:
-                # Replace references to local files with links to the GitHub repo
-                #
-                    newURI = join(githubFileURL, filePath)
-                    print("new url: ", newURI)
-                    node['refuri']=newURI
-                else:
-                # If there are links to local files other than .md (.rst files are caught
-                # when warnings are fired), move the files into the Sphinx project, so
-                # they can be accessed.
-                    newFileDir = join(app.outdir,dirname(filePath)) # where to move the file in Sphinx output.
-                    newFilePath = join(app.outdir,filePath)
-                    newURI = uri # if the path is relative no need to change it.
-                    if uri.startswith("/"):
-                    # It's an absolute path. Need to make it relative.
-                        uri = uri.lstrip("/")
-                        docDirDepth = len(docname.split("/")) - 1
-                        newURI = "../"*docDirDepth + uri
-                    if not isdir(newFileDir):
-                        makedirs(newFileDir)
-                    copyfile(filePath,newFilePath)
-                    node['refuri'] = newURI
-        elif "#" not in uri: # ignore anchors
-        # turn links to directories into links to the repo
-            if isdir(filePath):
-                newURI = join(githubDirURL, filePath)
-                node['refuri']=newURI
