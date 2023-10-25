cecc41241b5a6e72ea2fa2a1866f1e0de996c098
Github <no-reply@github.com>
2023-10-25 11:31:49 +0000

    Update documentation for v0.2
    Auto-generated from v0.2.1-13-gb091248 by 'update-gh-pages.sh'

--- v0.2/_static/documentation_options.js
+++ v0.2/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.2.1-11-g95e1402',
+    VERSION: 'v0.2.1-13-gb091248',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
