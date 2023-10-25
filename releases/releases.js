5c0068c1b1b2b76697af1006c1cec13488264de2
Github <no-reply@github.com>
2023-10-25 13:05:18 +0000

    Update documentation for v0.2
    Auto-generated from v0.2.1-17-g4e91e62 by 'update-gh-pages.sh'

--- releases/releases.js
+++ releases/releases.js
@@ -3,5 +3,6 @@ function getReleaseListItems() {
     { name: 'v0.1.0', url: 'v0.1.0' },
     { name: 'v0.2.0', url: 'v0.2.0' },
     { name: 'v0.2.1', url: 'v0.2.1' },
+    { name: 'v0.2.2', url: 'v0.2.2' },
   ];
 }
