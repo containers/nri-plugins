da84ed88eb20b050b559e17632e05a337fcf52c9
Github <no-reply@github.com>
2023-10-27 08:24:15 +0000

    Update documentation for v0.2
    Auto-generated from v0.2.2-6-g1890d14 by 'update-gh-pages.sh'

--- v0.2/_static/documentation_options.js
+++ v0.2/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.2.2-4-gbc777a0',
+    VERSION: 'v0.2.2-6-g1890d14',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
