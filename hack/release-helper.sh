5804e9a626376b7abb641dbb5f24c3b14ba38f96
Feruzjon Muyassarov <feruzjon.muyassarov@intel.com>
2023-12-29 11:37:45 +0200

    patch operator manifests before the release
    Ensure that operator manifests are pointing to the right tag/version
    before pushing a new tag.
    
    Signed-off-by: Feruzjon Muyassarov <feruzjon.muyassarov@intel.com>

--- hack/release-helper.sh
+++ hack/release-helper.sh
@@ -22,6 +22,8 @@ fi
 version=$1
 shift 1
 
+container_image=ghcr.io/containers/nri-plugins/nri-plugins-operator:$version
+
 if ! [[ $version =~ ^v[0-9]+\.[0-9]+\..+$ ]]; then
     echo -e "ERROR: invalid VERSION '$version'"
     exit 1
@@ -34,3 +36,9 @@ find deployment/helm -name Chart.yaml | xargs -I '{}' \
 find deployment/helm -name values.yaml | xargs -I '{}' \
     sed -e s"/pullPolicy:.*/pullPolicy: IfNotPresent/" -i '{}'
 
+# Patch deployment templates
+echo Patching kustomize templates to use $container_image
+find deployment/operator/config/manifests/bases -name nri-plugins-operator.clusterserviceversion.yaml | xargs -I '{}' \
+    sed -E -e s",^([[:space:]]+)containerImage:.+$,\1containerImage: $container_image," -i '{}'
+find deployment/operator/config/manager -name kustomization.yaml | xargs -I '{}' \
+    sed -E -e s",^([[:space:]]+)newTag:.+$,\1newTag: $version," -i '{}'
