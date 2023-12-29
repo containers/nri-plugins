f0be60f3cc68e70a9b02b21dbeeeecff3cb16473
Github <no-reply@github.com>
2023-12-29 10:09:50 +0000

    Update documentation for v0.3
    Auto-generated from v0.3.1 by 'update-gh-pages.sh'

--- releases/releases.js
+++ releases/releases.js
@@ -7,5 +7,6 @@ function getReleaseListItems() {
     { name: 'v0.2.3', url: 'v0.2.3' },
     { name: 'v0.3.0', url: 'v0.3.0' },
     { name: 'v0.3.1', url: 'v0.3.1' },
+    { name: 'v0.3.2', url: 'v0.3.2' },
   ];
 }
