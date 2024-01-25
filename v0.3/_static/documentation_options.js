08c6fc794030758a095910154e890bdfee5603e3
Github <no-reply@github.com>
2024-01-25 17:09:18 +0000

    Update documentation for v0.3
    Auto-generated from v0.3.2-21-gd3f3ac6 by 'update-gh-pages.sh'

--- v0.3/_static/documentation_options.js
+++ v0.3/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.3.2-20-gdd67c5d',
+    VERSION: 'v0.3.2-21-gd3f3ac6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
