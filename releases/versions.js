f067ff1cee38b4c1a96be30dc4f8b59a9cda4b04
Github <no-reply@github.com>
2024-03-15 14:54:08 +0000

    Update documentation for v0.4
    Auto-generated from v0.4.0-1-g659d042 by 'update-gh-pages.sh'

--- releases/versions.js
+++ releases/versions.js
@@ -5,5 +5,6 @@ function getVersionsMenuItems() {
     { name: 'v0.1', url: '/nri-plugins/v0.1' },
     { name: 'v0.2', url: '/nri-plugins/v0.2' },
     { name: 'v0.3', url: '/nri-plugins/v0.3' },
+    { name: 'v0.4', url: '/nri-plugins/v0.4' },
   ];
 }
