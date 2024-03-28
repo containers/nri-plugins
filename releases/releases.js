8a32f28614e8bff99bf6b4a2330d4316177844cf
Github <no-reply@github.com>
2024-03-28 21:10:45 +0000

    Update documentation for v0.5
    Auto-generated from v0.5.1 by 'update-gh-pages.sh'

--- releases/releases.js
+++ releases/releases.js
@@ -11,5 +11,6 @@ function getReleaseListItems() {
     { name: 'v0.4.1', url: 'v0.4.1' },
     { name: 'v0.5.0-devel', url: 'v0.5.0-devel' },
     { name: 'v0.5.0', url: 'v0.5.0' },
+    { name: 'v0.5.1', url: 'v0.5.1' },
   ];
 }
