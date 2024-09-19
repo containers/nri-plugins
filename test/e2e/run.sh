c853c135cd19a27849f5fd51a11dd56bb7c1e50a
Krisztian Litkey <krisztian.litkey@intel.com>
2024-09-19 11:14:49 +0300

    e2e: use bridge CNI by default.
    Switch to using the bridge CNI plugin by default. With the latest
    playbook fixes it should work now and it is the fastest to set up.
    The latest cilium release is hosed, so it's a good time to switch:
      https://github.com/cilium/cilium-cli/issues/2795
    
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -19,7 +19,7 @@ export COMMAND_OUTPUT_DIR="$TEST_OUTPUT_DIR"/commands
 
 distro=${distro:-$DEFAULT_DISTRO}
 export k8scri=${k8scri:-"containerd"}
-export cni_plugin=${cni_plugin:-cilium}
+export cni_plugin=${cni_plugin:-bridge}
 export cni_release=${cni_release:-latest}
 TOPOLOGY_DIR=${TOPOLOGY_DIR:=e2e}
 
