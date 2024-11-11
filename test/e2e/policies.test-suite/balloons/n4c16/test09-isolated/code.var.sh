8c89a0449b4e9ad63c678f6a105b7071cb2173a6
Krisztian Litkey <krisztian.litkey@intel.com>
2024-11-11 22:12:30 +0200

    e2e/balloons: remove unknown/unused helm-launch argument.
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

--- test/e2e/policies.test-suite/balloons/n4c16/test09-isolated/code.var.sh
+++ test/e2e/policies.test-suite/balloons/n4c16/test09-isolated/code.var.sh
@@ -1,5 +1,5 @@
 helm-terminate
-helm_config=${TEST_DIR}/balloons-isolated.cfg nri_resource_policy_extra_args="-metrics-interval 4s" helm-launch balloons
+helm_config=${TEST_DIR}/balloons-isolated.cfg helm-launch balloons
 
 verify-metrics-has-line 'balloon="isolated-pods\[0\]"'
 verify-metrics-has-line 'balloon="isolated-pods\[1\]"'
