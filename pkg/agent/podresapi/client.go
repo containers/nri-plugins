70831663531460cf4cbe5755265bec8fd207292a
Krisztian Litkey <krisztian.litkey@intel.com>
2024-11-30 15:24:36 +0200

    agent: add support for querying Pod Resource API.
    Add initial support for querying the kubelet pod resource
    API for extra information about resources (and devices in
    particular) assigned to pods and containers.
    
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

---
+++ pkg/agent/podresapi/client.go
@@ -0,0 +1,165 @@
+// Copyright The NRI Plugins Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package podresapi
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"strings"
+	"time"
+
+	logger "github.com/containers/nri-plugins/pkg/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	api "k8s.io/kubelet/pkg/apis/podresources/v1"
+)
+
+// ClientOption is an option for the client.
+type ClientOption func(*Client)
+
+// Client is Pod Resources API client.
+type Client struct {
+	socketPath string
+	conn       *grpc.ClientConn
+	cli        api.PodResourcesListerClient
+	noGet      bool
+}
+
+const (
+	// these constants were obtained from NFD sources, cross-checked against
+	//   https://github.com/kubernetes/kubernetes/blob/release-1.31/test/e2e_node/util.go#L83
+	defaultSocketPath = "/var/lib/kubelet/pod-resources/kubelet.sock"
+	timeout           = 10 * time.Second
+	maxSize           = 1024 * 1024 * 16
+)
+
+var (
+	errGetDisabled = fmt.Errorf("PodResources API Get method disabled")
+	log            = logger.Get("podresapi")
+)
+
+// WithSocketPath sets the kubelet socket path to connect to.
+func WithSocketPath(path string) ClientOption {
+	return func(c *Client) {
+		c.socketPath = path
+	}
+}
+
+// WithClientConn sets a pre-created gRPC connection for the client.
+func WithClientConn(conn *grpc.ClientConn) ClientOption {
+	return func(c *Client) {
+		c.conn = conn
+	}
+}
+
+// NewClient creates a new Pod Resources API client with the given options.
+func NewClient(options ...ClientOption) (*Client, error) {
+	c := &Client{
+		socketPath: defaultSocketPath,
+	}
+
+	for _, o := range options {
+		o(c)
+	}
+
+	if c.conn == nil {
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		dialer := func(ctx context.Context, addr string) (net.Conn, error) {
+			return (&net.Dialer{}).DialContext(ctx, "unix", addr)
+		}
+
+		conn, err := grpc.DialContext(
+			ctx,
+			c.socketPath,
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+			grpc.WithContextDialer(dialer),
+			grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(maxSize)),
+			grpc.WithBlock(),
+		)
+
+		if err != nil {
+			return nil, fmt.Errorf("failed to connect podresource client: %w", err)
+		}
+
+		c.conn = conn
+	}
+
+	c.cli = api.NewPodResourcesListerClient(c.conn)
+	return c, nil
+}
+
+// Close closes the client.
+func (c *Client) Close() {
+	if c != nil && c.conn != nil {
+		c.conn.Close()
+		c.conn = nil
+	}
+	c.cli = nil
+}
+
+// HasClient returns true if the client has a usable client.
+func (c *Client) HasClient() bool {
+	return c != nil && c.cli != nil
+}
+
+// List lists all pods' resources.
+func (c *Client) List(ctx context.Context) (PodResourcesList, error) {
+	if !c.HasClient() {
+		return nil, nil
+	}
+
+	reply, err := c.cli.List(ctx, &api.ListPodResourcesRequest{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get pod resources by list: %w", err)
+	}
+
+	return PodResourcesList(reply.GetPodResources()), nil
+}
+
+// Get queries the given pod's resources.
+func (c *Client) Get(ctx context.Context, namespace, pod string) (*PodResources, error) {
+	if !c.HasClient() {
+		return nil, nil
+	}
+
+	if !c.noGet {
+		reply, err := c.cli.Get(ctx, &api.GetPodResourcesRequest{
+			PodNamespace: namespace,
+			PodName:      pod,
+		})
+		if err == nil {
+			return &PodResources{reply.GetPodResources()}, nil
+		}
+
+		if !strings.Contains(fmt.Sprintf("%v", err), fmt.Sprintf("%v", errGetDisabled)) {
+			return nil, fmt.Errorf("failed to get pod resources: %w", err)
+		}
+
+		log.Warnf("PodResources API Get() disabled, falling back to List()...")
+
+		c.noGet = true
+	}
+
+	l, err := c.List(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get pod resources: %w", err)
+	}
+
+	return l.GetPodResources(namespace, pod), nil
+}
