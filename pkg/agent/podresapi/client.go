3fb1ab2fdd5898e5f306e6774e86e5d738ffd6bd
Krisztian Litkey <krisztian.litkey@intel.com>
2024-12-04 13:25:36 +0200

    agent,resmgr: merge PodResources{List,Map}, cache list.
    Merge PodResourceMap into PodResourcesList. Avoid unnecessary
    re-listing of pod resources by caching the result of the last
    list and trying to reuse it to look up resources.
    
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

--- pkg/agent/podresapi/client.go
+++ pkg/agent/podresapi/client.go
@@ -35,6 +35,7 @@ type Client struct {
 	conn       *grpc.ClientConn
 	cli        api.PodResourcesListerClient
 	noGet      bool
+	cached     *PodResourcesList
 }
 
 const (
@@ -105,7 +106,7 @@ func (c *Client) HasClient() bool {
 }
 
 // List lists all pods' resources.
-func (c *Client) List(ctx context.Context) (PodResourcesList, error) {
+func (c *Client) List(ctx context.Context) (*PodResourcesList, error) {
 	if !c.HasClient() {
 		return nil, nil
 	}
@@ -115,7 +116,9 @@ func (c *Client) List(ctx context.Context) (PodResourcesList, error) {
 		return nil, fmt.Errorf("failed to get pod resources by list: %w", err)
 	}
 
-	return PodResourcesList(reply.GetPodResources()), nil
+	c.cached = NewPodResourcesList(reply.GetPodResources())
+
+	return c.cached, nil
 }
 
 // Get queries the given pod's resources.
@@ -142,6 +145,10 @@ func (c *Client) Get(ctx context.Context, namespace, pod string) (*PodResources,
 		log.Warnf("  --feature-gates=KubeletPodResourcesGet=true")
 
 		c.noGet = true
+	}
+
+	if r := c.cached.GetPodResources(namespace, pod); r != nil {
+		return r, nil
 	}
 
 	l, err := c.List(ctx)
