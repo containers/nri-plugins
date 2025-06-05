c8e61034df7d76586c9f3dfb2410b8b77e274658
Krisztian Litkey <krisztian.litkey@intel.com>
2025-06-05 23:32:39 +0300

    WiP: topology-aware: publish CPUs as DRA devices.
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

--- cmd/plugins/topology-aware/policy/topology-aware-policy.go
+++ cmd/plugins/topology-aware/policy/topology-aware-policy.go
@@ -145,6 +145,10 @@ func (p *policy) Start() error {
 	p.root.Dump("<post-start>")
 	p.checkAllocations("  <post-start>")
 
+	if err := p.options.PublishCPUs(p.allowed.Difference(p.reserved).List()); err != nil {
+		log.Errorf("failed to publish CPU DRA resources: %v", err)
+	}
+
 	return nil
 }
 
@@ -497,6 +501,10 @@ func (p *policy) Reconfigure(newCfg interface{}) error {
 
 	p.root.Dump("<post-config>")
 	p.checkAllocations("  <post-config>")
+
+	if err := p.options.PublishCPUs(p.allowed.Difference(p.reserved).List()); err != nil {
+		log.Errorf("failed to publish CPU DRA resources: %v", err)
+	}
 
 	return nil
 }
