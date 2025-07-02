d70a37981bb3098e615f972b7dda567283a7af11
Github <no-reply@github.com>
2025-07-02 10:54:21 +0000

    Update documentation for v0.10
    Auto-generated from v0.10.0 by 'update-gh-pages.sh'

--- releases/releases.js
+++ releases/releases.js
@@ -1,6 +1,7 @@
 function getReleaseListItems() {
   return [
     { name: 'v0.1.0', url: 'v0.1.0' },
+    { name: 'v0.10.0', url: 'v0.10.0' },
     { name: 'v0.2.0', url: 'v0.2.0' },
     { name: 'v0.2.1', url: 'v0.2.1' },
     { name: 'v0.2.2', url: 'v0.2.2' },
