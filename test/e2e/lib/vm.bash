4ed617e0ab14305a2ce1a60f68cfeaa2d71ecb14
Antti Kervinen <antti.kervinen@intel.com>
2025-12-12 16:06:33 +0200

    WIP: vm-kernel-pkgs-install do grubby manually for ubuntu

--- test/e2e/lib/vm.bash
+++ test/e2e/lib/vm.bash
@@ -768,19 +768,38 @@ vm-kernel-pkgs-install() { # script API
         return 0
     fi
 
-    # TODO: is grubby available on Ubuntu?
-    vm-command "grubby --default-kernel | tee .$feat.default-kernel" || \
-        command-error "cannot save current default kernel"
-
     vm-command "[ -d $feat ] && rm -rf $feat; mkdir $feat; tar xvf $file -C $feat | tee .$feat.extracted_packages" ||
         command-error "cannot extract kernel packages from $file"
 
     if grep -q .rpm <<< "$to_be_installed"; then
+        # Store current kernel / Fedora
+        vm-command "grubby --default-kernel | tee .$feat.default-kernel" || \
+            command-error "cannot save current default kernel"
+        # Install new kernel packages / Fedora
         vm-command "rpm -Uvh $feat/*.rpm" || \
             command-error "cannot install kernel rpm packages from $file"
     elif grep -q .deb <<< "$to_be_installed"; then
+        # Store current kernel / Ubuntu
+        vm-command "uname -r | tee .$feat.default-kernel"
+        # Install new kernel packages / Ubuntu
         vm-command "dpkg -i $feat/*.deb" || \
             command-error "cannot install kernel deb packages from $file"
+        # In lack of grubby, set GRUB_DEFAULT in both "original" (for current) and "override" (for new kernel) configs.
+        # They both boot the system with current kernel and new kernel, respectively.
+        # When wish to change back from the new to the original kernel, it suffices to remove the "override" config
+        # and update grub.
+        vm-command "
+            oldkernelversion=\$(uname -r)
+            newkernelversion=\$(dpkg --contents $feat/linux-image-*.deb | awk -Fvmlinuz- /vmlinuz-/'{print \$2}')
+            submenu=\$(grep -i submenu.*advanced /boot/grub/grub.cfg | awk -F\' '{print \$2}')
+            oldmenuchoice=\$(grep \"menuentry.*\$oldkernelversion\" /boot/grub/grub.cfg | grep -v recovery | awk -F\' '{print \$2}')
+            newmenuchoice=\$(grep \"menuentry.*\$newkernelversion\" /boot/grub/grub.cfg | grep -v recovery | awk -F\' '{print \$2}')
+            echo 'Save original grub default'
+            echo \"GRUB_DEFAULT='\$submenu>\$oldmenuchoice'\" | tee /etc/default/grub.d/e2e-00-original-default-kernel.cfg
+            echo 'Override original grub default with:'
+            echo \"GRUB_DEFAULT='\$submenu>\$newmenuchoice'\" | tee /etc/default/grub.d/e2e-01-override-default-kernel.cfg
+            update-grub
+            "
     else
         command-error "no kernel packages found in $file"
     fi
@@ -797,14 +816,16 @@ vm-kernel-pkgs-install() { # script API
 vm-kernel-pkgs-uninstall() { # script API
     # Usage: vm-kernel-pkgs-uninstall
     #
-    # Boot to previous kernel before vm-kernel-pkgs-install(s) and
-    # uninstall the custom kernel packages installed with
-    # vm-kernel-pkgs-install.
+    # Boot to previous kernel before vm-kernel-pkgs-install and
+    # uninstall custom kernel packages installed with vm-kernel-pkgs-install.
     local feat="vm-kernel-pkgs"
     local default_kernel
     local installed_packages
-    vm-command "cat .$feat.default-kernel" || \
-        command-error "cannot find previous default kernel file ~/.$feat.default-kernel"
+    vm-command "cat .$feat.default-kernel" || {
+        echo "vm-kernel-pkgs-uninstall: kernel-pkgs not installed"
+        return 0
+    }
+
     default_kernel="$COMMAND_OUTPUT"
     if [ -z "$default_kernel" ]; then
         command-error "cannot restore previous default kernel, file ~/.$feat.default-kernel is missing or empty"
@@ -817,9 +838,13 @@ vm-kernel-pkgs-uninstall() { # script API
         command-error "cannot uninstall kernel packages, file ~/.$feat.installed_packages is missing or empty"
     fi
 
-    # Reboot to previous kernel, otherwise uninstalling might fail.
-    vm-command "grubby --set-default=\"$default_kernel\"" || \
-        command-error "cannot restore previous default kernel to $default_kernel"
+    # Modify grub to select previous kernel on next boot
+    if grep -q rpm <<< "$installed_packages"; then
+        vm-command "grubby --set-default=\"$default_kernel\"" || \
+            command-error "cannot restore previous default kernel to $default_kernel"
+    else
+        vm-command "rm -f /etc/default/grub.d/e2e-01-override-default-kernel.cfg; update-grub"
+    fi
 
     echo "Booting to previous kernel: $default_kernel."
     vm-reboot
@@ -831,6 +856,7 @@ vm-kernel-pkgs-uninstall() { # script API
     elif grep -q deb <<< "$installed_packages"; then
         vm-command "xargs -a .$feat.installed_packages dpkg -r" || \
             command-error "failed to uninstall packages: $installed_packages"
+        vm-command "rm -f /etc/default/grub.d/e2e-00-original-default-kernel.cfg; update-grub"
     else
         command-error "no kernel packages found in ~/.$feat.installed_packages"
     fi
