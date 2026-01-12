573eab85e5f7335a4a22ee707122a7c1f7ebc324
Krisztian Litkey <krisztian.litkey@intel.com>
2026-01-12 14:20:28 +0200

    e2e: bump default test distro to fedora/43.
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

--- test/e2e/run_tests.sh
+++ test/e2e/run_tests.sh
@@ -4,7 +4,7 @@ TESTS_DIR="$1"
 SKIP_LONG_TESTS="${skip_long_tests:-yes}"
 RUN_SH="${0%/*}/run.sh"
 
-DEFAULT_DISTRO=${DEFAULT_DISTRO:-"fedora/42"}
+DEFAULT_DISTRO=${DEFAULT_DISTRO:-"fedora/43"}
 
 allow_var_override=""
 
