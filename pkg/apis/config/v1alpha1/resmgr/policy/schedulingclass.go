8c2a56fb183ca5ed777b5e7613405605b4c5f69c
Antti Kervinen <antti.kervinen@intel.com>
2026-01-23 17:08:30 +0200

    balloons: implement scheduling classes
    Signed-off-by: Antti Kervinen <antti.kervinen@intel.com>

--- pkg/apis/config/v1alpha1/resmgr/policy/schedulingclass.go
+++ pkg/apis/config/v1alpha1/resmgr/policy/schedulingclass.go
@@ -25,19 +25,30 @@ type SchedulingClass struct {
 	// SCHED_<NAME> translates to <name> etc.
 	// +kubebuilder:validation:Enum=none;other;fifo;rr;batch;iso;idle;deadline
 	// +kubebuilder:validation:Format:string
-	Policy   SchedulingPolicy `json:"policy,omitempty"`
-	Priority *int             `json:"priority,omitempty"`
+	Policy SchedulingPolicy `json:"policy,omitempty"`
+	// Priority is the Linux scheduling priority to use.
+	// Valid range depends on the selected policy.
+	// Refer to sched_setscheduler(2) for details.
+	Priority *int `json:"priority,omitempty"`
 	// Flags is a list of Linux scheduling flags to set.
 	// SCHED_FLAG_<ORIG_NAME> translates to <orig-name> etc.
 	// +kubebuilder:validation:Enum=reset-on-fork;reclaim;dl-overrun;keep-policy;keep-params;util-clamp-min;util-clamp-max
-	Flags    SchedulingFlags `json:"flags,omitempty"`
-	Nice     *int            `json:"nice,omitempty"`
-	Runtime  *uint64         `json:"runtime,omitempty"`
-	Deadline *uint64         `json:"deadline,omitempty"`
-	Period   *uint64         `json:"period,omitempty"`
-
+	Flags SchedulingFlags `json:"flags,omitempty"`
+	// Nice is the Linux nice value to use.
+	Nice *int `json:"nice,omitempty"`
+	// Runtime is the Linux SCHED_DEADLINE runtime value to use (in microseconds).
+	Runtime *uint64 `json:"runtime,omitempty"`
+	// Deadline is the Linux SCHED_DEADLINE deadline value to use (in microseconds).
+	Deadline *uint64 `json:"deadline,omitempty"`
+	// Period is the Linux SCHED_DEADLINE period value to use (in microseconds).
+	Period *uint64 `json:"period,omitempty"`
 	// IOClass is the IO scheduling class to use.
+	// IOPRIO_CLASS_<NAME> translates to <name>.
+	// Refer to ioprio_set(2) and ionice(1) for details.
 	// +kubebuilder:validation:Enum=none;rt;be;idle
-	IOClass    IOPriorityClass `json:"ioClass,omitempty"`
-	IOPriority *int            `json:"ioPriority,omitempty"`
+	IOClass IOPriorityClass `json:"ioClass,omitempty"`
+	// IOPriority is the IO priority within the selected IO class to use.
+	// Valid range depends on the selected class.
+	// Refer to ionice(1) for details.
+	IOPriority *int `json:"ioPriority,omitempty"`
 }
