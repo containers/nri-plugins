4767bf628c54bc73381a26f0049605d7f2045df2
Krisztian Litkey <krisztian.litkey@intel.com>
2026-04-14 20:18:36 +0300

    log: rip out old logger and plug in slog-based replacement.
    Rework internal logger implementation using slog, allowing us
    to eventually move to structured logging.
    
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

--- go.mod
+++ go.mod
@@ -31,7 +31,6 @@ require (
 	go.opentelemetry.io/otel/sdk/metric v1.44.0
 	go.opentelemetry.io/otel/trace v1.44.0
 	golang.org/x/sys v0.45.0
-	golang.org/x/time v0.9.0
 	google.golang.org/grpc v1.81.1
 	k8s.io/api v0.31.2
 	k8s.io/apimachinery v0.33.1
@@ -92,6 +91,7 @@ require (
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/term v0.43.0 // indirect
 	golang.org/x/text v0.37.0 // indirect
+	golang.org/x/time v0.9.0 // indirect
 	golang.org/x/tools v0.44.0 // indirect
 	golang.org/x/tools/go/packages/packagestest v0.1.1-deprecated // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20260526163538-3dc84a4a5aaa // indirect
---
+++ pkg/log/logger.go
@@ -0,0 +1,74 @@
+// Copyright The NRI Plugins Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package log
+
+import (
+	"context"
+	"log/slog"
+)
+
+const (
+	DebugEnvVar       = "LOGGER_DEBUG"
+	LogSourceEnvVar   = "LOGGER_LOG_SOURCE"
+	LogSkipHdrsEnvVar = "LOGGER_SKIP_HEADERS"
+)
+
+type Level = slog.Level
+
+const (
+	LevelDebug = slog.LevelDebug
+	LevelInfo  = slog.LevelInfo
+	LevelWarn  = slog.LevelWarn
+	LevelError = slog.LevelError
+	LevelFatal = slog.LevelError + 4
+	LevelPanic = slog.LevelError + 8
+)
+
+type Attr = slog.Attr
+
+type Logger interface {
+	Log(lvl Level, msg string, attrs ...Attr)
+	Debug(msg string, attrs ...Attr)
+	Info(msg string, attrs ...Attr)
+	Warn(msg string, attrs ...Attr)
+	Error(msg string, attrs ...Attr)
+	Fatal(msg string, attrs ...Attr)
+	Panic(msg string, attrs ...Attr)
+
+	Debugf(format string, args ...any)
+	Infof(format string, args ...any)
+	Warnf(format string, args ...any)
+	Errorf(format string, args ...any)
+	Fatalf(format string, args ...any)
+	Panicf(format string, args ...any)
+
+	LogBlock(lvl Level, prefix, format string, args ...any)
+	DebugBlock(prefix string, format string, args ...any)
+	InfoBlock(prefix string, format string, args ...any)
+	WarnBlock(prefix string, format string, args ...any)
+	ErrorBlock(prefix string, format string, args ...any)
+
+	WithContext(ctx context.Context) Logger
+	WithGroup(name string) Logger
+	WithAttrs(args ...any) Logger
+
+	Enabled(ctx context.Context, lvl Level) bool
+
+	DebugEnabled() bool
+	EnableDebug(bool) bool
+
+	Flush()
+	SlogHandler() slog.Handler
+}
